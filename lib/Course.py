92a62bbf61a6d0581e7072c9893e5ec7d4c2d1d8
Peter Emu <emu.kimani@strathmore.edu>
2026-01-12 20:38:53 +0300

    lab done

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,8 @@
 class Course:
-    pass
+    def __init__(self, title='', schedule='', description=''):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        return f"Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n------------------"
--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 from Course import Course
-import ipdb
-
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        doc = BeautifulSoup(
+            requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text,
+            'html.parser'
+        )
+        return doc
+
+    def get_courses(self):
+        return self.get_page().select('.post')
+
+    def make_courses(self):
+        for course in self.get_courses():
+            # Skip empty placeholder courses
+            if not course.select("h2"):
+                continue
+
+            title = course.select("h2")[0].text
+            schedule = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text if course.select("p") else ''
+            
+            new_course = Course(title, schedule, description)
+            self.courses.append(new_course)
+        return self.courses
+
+    def print_courses(self):
+        for course in self.make_courses():
+            print(course)
+
+# Run this to see the courses
+if __name__ == "__main__":
+    Scraper().print_courses()
